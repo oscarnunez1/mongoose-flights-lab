1b8e8f98e3041448b532c94762c407a79fae1567
Oscar Nunez <requinto31@gmail.com>
2023-01-10 13:10:35 -0500

    import config database in server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 
+
+import './config/database.js'
+
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/users.js'
