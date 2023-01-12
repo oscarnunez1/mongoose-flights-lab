586b1f87297a8f8213362dfd05d959df563af9f9
Oscar Nunez <requinto31@gmail.com>
2023-01-12 10:03:35 -0500

    organize routers from specificity

--- routes/flights.js
+++ routes/flights.js
@@ -3,18 +3,17 @@ import * as flightsCtrl from "../controllers/flights.js"
 
 const router = Router()
 
-// GET localhost:3000/flights
 router.post('/', flightsCtrl.create)
 router.post('/:id/tickets', flightsCtrl.createTicket)
 
 router.get('/', flightsCtrl.index)
-router.get('/:id', flightsCtrl.show)
 router.get('/new', flightsCtrl.new)
+router.get('/:id', flightsCtrl.show)
 router.get('/:id/edit', flightsCtrl.edit)
 
+router.put('/:id', flightsCtrl.update)
 router.delete('/:id', flightsCtrl.delete)
 
-router.put('/:id', flightsCtrl.update)
 
 export {
   router
